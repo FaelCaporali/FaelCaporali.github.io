eae4052e6a21e3827326cc4ca19e766e6ed04ba3
FaelCaporali <rafaelhon@gmail.com>
2022-04-12 13:40:40 -0300

    location initializer

--- script.js
+++ script.js
@@ -185,4 +185,5 @@ window.onload = () => {
   window.alert(
     "Este é um portfólio dinâmico e em construção, não se assuste se alguma funcionalidade estiver quebrada. Para uma melhor experiência, por enquanto navegue em dispositivos com telas pequenas. =]"
   );
+  window.location.href = '#home-chaser';
 };
